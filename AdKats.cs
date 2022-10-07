b22311566628170fd1baa91aa431760cedccfcd3
Hedius <git@hedius.eu>
2022-10-07 02:31:41 +0200

    raise version to 8.1.5.0 - test release

--- AdKats.cs
+++ AdKats.cs
@@ -21,11 +21,11 @@
  * Work on fork by Hedius (Version >= 8.0.0.0)
  * 
  * AdKats.cs
- * Version 8.1.4.0
- * 15-AUG-2022
+ * Version 8.1.5.0
+ * 07-OCT-2022
  * 
  * Automatic Update Information
- * <version_code>8.1.4.0</version_code>
+ * <version_code>8.1.5.0</version_code>
  */
 
 using System;
@@ -68,7 +68,7 @@ namespace PRoConEvents
     {
 
         //Current Plugin Version
-        private const String PluginVersion = "8.1.4.0";
+        private const String PluginVersion = "8.1.5.0";
 
         public enum GameVersionEnum
         {
